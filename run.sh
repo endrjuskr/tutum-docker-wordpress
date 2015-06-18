9ffa84c48cba5cd288a9843628c525350347e8bd
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-06-18 08:26:37 +0200

    Change order.

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> Installing MySQL ..."
     mysql_install_db > /dev/null 2>&1
     echo "=> Done!"  
-    /create_mysql_admin_user.sh
+    echo "=> Creating wp site"
+	php /wp-cli.phar core install --allow-root --path=/app/ --url=http://localhost:80 --title=test --admin_name=endrju --admin_email=endrju@endrju.pl --admin_password=test1234
+	echo "=> Done"
+	/create_mysql_admin_user.sh
+    
 else
     echo "=> Using an existing volume of MySQL"
 fi
-
-echo "=> Creating wp site"
-php /wp-cli.phar core install --allow-root --path=/app/ --url=http://localhost:80 --title=test --admin_name=endrju --admin_email=endrju@endrju.pl --admin_password=test1234
-echo "=> Done"
 
 exec supervisord -n
