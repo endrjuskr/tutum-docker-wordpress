96a42f4e96585989a53703ea7b7860f6b63418e7
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-06-18 09:04:57 +0200

    Reorder.

--- run.sh
+++ run.sh
@@ -15,11 +15,11 @@ else
     echo "=> Using an existing volume of MySQL"
 fi
 
-sleep 20
+sleep 60
 
 echo "=> Creating wp site"
 php /wp-cli.phar core install --allow-root --path=/app --url=http://localhost --title=test --admin_name=endrju --admin_email=endrju@endrju.pl --admin_password=test1234
 echo "=> Done"
 
-exec supervisord -n
+# exec supervisord -n
 	
